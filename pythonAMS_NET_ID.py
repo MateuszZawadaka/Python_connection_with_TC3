c247c064f7398dd54ccc9a1bb0a6e1a4c1d32c0e
Mateusz Zawadka <mateusz.zawadka@atmat.pl>
2023-12-06 17:03:51 +0100

    Add method for read value in real time

--- pythonAMS_NET_ID.py
+++ pythonAMS_NET_ID.py
@@ -1,6 +1,6 @@
 import pyads
 import customtkinter
-
+import time
 
 root_tk = customtkinter.CTk()
 root_tk.geometry("1920x1080")
@@ -12,46 +12,92 @@ plc_address_input = customtkinter.CTkEntry(master=root_tk, width=190, height=20,
 plc_address_input.place(relx=0.1, rely=0.15, anchor=customtkinter.CENTER)
 plc_port_input = customtkinter.CTkEntry(master=root_tk, width=190, height=20, corner_radius=5, placeholder_text="PLC PORT:" )
 plc_port_input.place(relx=0.1, rely=0.2, anchor=customtkinter.CENTER)
-variabe_name = customtkinter.CTkEntry(master=root_tk, width=190, height=20, corner_radius=5, placeholder_text="Variable name:")
-variabe_name.place(relx=0.1, rely=0.25, anchor=customtkinter.CENTER)
+variable_path = customtkinter.CTkEntry(master=root_tk, width=190, height=20, corner_radius=5, placeholder_text="Variable name:")
+variable_path.place(relx=0.1, rely=0.25, anchor=customtkinter.CENTER)
+value_to_write_input = customtkinter.CTkEntry(master=root_tk, width=190, height=20, corner_radius=5, placeholder_text="Value to wirite")
+value_to_write_input.place(relx=0.9, rely=0.27, anchor=customtkinter.CENTER)
 #plc_address= "192.168.17.156.1.1"
 #, plc_port= 851
-#variable_name = "GvlInputs.iVar"
+#variable_name = "GvlInputs.general.iInt"
 def open_connection():
     plc_address = plc_address_input.get()
     plc_port = int(plc_port_input.get())
-    variable_name = variabe_name.get()
+    variable_name = variable_path.get()
     try:
-        # Open a connection to the TwinCAT PLC
         with pyads.Connection(plc_address, plc_port) as plc:
             plc.open()
-            # Your code here (e.g., read/write operations)
             value =  plc.read_by_name(data_name = variable_name, plc_datatype = pyads.PLCTYPE_INT) 
             print(f"Connection to {plc_address}:{plc_port} established.")
             variable_value = customtkinter.CTkLabel(master=root_tk, text=(f"Value of {variable_name}: {value}"), width=120, height=20, corner_radius=8, text_color="white", bg_color="blue")   
-            variable_value.place(relx=0.4, rely=0.2, anchor=customtkinter.CENTER)
+            variable_value.place(relx=0.4, rely=0.1, anchor=customtkinter.CENTER)
             
             # ...
     except pyads.ADSError as e:
         print(f"Failed to open connection: {e}")
         error_message_label = customtkinter.CTkLabel(master=root_tk, text=f"Failed to open connection: {e}", width=120, height=20, corner_radius=1, text_color="white", bg_color="red")
         error_message_label.place(relx=0.4, rely=0.05, anchor=customtkinter.CENTER)
-        #clear_error_message_button = customtkinter.CTkButton(master=root_tk, text="Delete error message", corner_radius=10, command=error_message_label.after(1000, error_message_label.destroy()))
-        #clear_error_message_button.place(relx = 0.4, rely = 0.1)
 
 def button_pressed():
     open_connection()
+
+def real_value_of_cpu_Usage(interval_s = 0.1):
+    plc_address = "192.168.17.156.1.1" #plc_address_input.get()
+    plc_port = int(851)
+    variable_name = "GvlOutputs.general.lrCpuUsage"
+    try:
+        with pyads.Connection(plc_address, plc_port) as plc:
+            plc.open()
+            value =  plc.read_by_name(data_name = variable_name, plc_datatype = pyads.PLCTYPE_LREAL) 
+            print(f"Connection to {plc_address}:{plc_port} established.")
+            cpu_suage = customtkinter.CTkLabel(master=root_tk, text=(f"Value of {variable_name}: {value}"), width=120, height=20, corner_radius=8, text_color="white", bg_color="blue")   
+            cpu_suage.place(relx=0.4, rely=0.2, anchor=customtkinter.CENTER)
+            time.sleep(interval_s)
+    except pyads.ADSError as e:
+        print(f"Failed to open connection: {e}")
+        error_message_label = customtkinter.CTkLabel(master=root_tk, text=f"Failed to open connection: {e}", width=120, height=20, corner_radius=1, text_color="white", bg_color="red")
+        error_message_label.place(relx=0.4, rely=0.05, anchor=customtkinter.CENTER)
     
+
+def write_data_to_variable():
+    plc_address = plc_address_input.get()
+    plc_port = int(plc_port_input.get())
+    variable_name = variable_path.get()
+    value_to_write = int(value_to_write_input.get())
+    try: 
+        with pyads.Connection(plc_address, plc_port) as plc:
+            plc.open()
+
+            result = plc.write_by_name(variable_name, int(value_to_write))
+
+            if result == 0:
+                print(f"{variable_name} set to {value_to_write}")
+            else:
+                print(f"Failed to write {variable_name}. Error code: {result}")
+
+    except pyads.ADSError as e:
+        print(f"Failed to open connection: {e}")
+        error_message_label = customtkinter.CTkLabel(master=root_tk, text=f"Failed to open connection: {e}", width=120, height=20, corner_radius=1, text_color="white", bg_color="red")
+        error_message_label.place(relx=0.4, rely=0.1, anchor=customtkinter.CENTER)
+
 
 def select_plc_parameters():
     print("Address: ",plc_address_input.get())
     print("Port: ", plc_port_input.get())
 
+def destroy():
+    root_tk.destroy()
+
 if __name__ == "__main__":
+    interval = 0.1
+    real_value_of_cpu_Usage(interval_s=interval)
     select_plc_parameters_button = customtkinter.CTkButton(master=root_tk, text="Select PLC paramters", corner_radius=10, command=select_plc_parameters)
-    select_plc_parameters_button.place(relx = 0.08, rely = 0.3, anchor=customtkinter.CENTER)
+    select_plc_parameters_button.place(relx = 0.1, rely = 0.3, anchor=customtkinter.CENTER)
     button = customtkinter.CTkButton(master=root_tk, text="Read variable value", corner_radius=10, command=button_pressed)
-    button.place(relx = 0.8, rely = 0.2, anchor=customtkinter.CENTER)
+    button.place(relx = 0.9, rely = 0.2, anchor=customtkinter.CENTER)
+    write_value_button = customtkinter.CTkButton(master=root_tk, text="Write value", corner_radius=10, command=write_data_to_variable)
+    write_value_button.place(relx=0.865, rely=0.3)
+    destroy_button= customtkinter.CTkButton(master=root_tk, text="Exit", corner_radius=10, command=destroy)
+    destroy_button.place(relx=0.9, rely=0.8)
     
     root_tk.mainloop()
 
